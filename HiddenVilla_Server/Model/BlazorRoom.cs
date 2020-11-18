864dd53de04445032f1b63fb37cec2353f6eac07
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-11-18 12:49:41 -0600

    Section 2 - Dropdown and Loops

--- HiddenVilla_Server/Model/BlazorRoom.cs
+++ HiddenVilla_Server/Model/BlazorRoom.cs
@@ -11,5 +11,6 @@ namespace HiddenVilla_Server.Model
         public string RoomName { get; set; }
         public double Price { get; set; }
         public bool IsActive { get; set; }
+        public List<BlazorRoomProp> RoomProps { get; set; }
     }
 }

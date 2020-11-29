50e452f4c01e32d400b7da0efc938f25fe66cb23
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-11-29 10:55:49 -0600

    Section 4 - Hotel Room Repostiroy Implementation Part 1

---
+++ Business/Repository/HotelRoomRepository.cs
@@ -0,0 +1,46 @@
+﻿using Business.Repository.IRepository;
+using DataAcesss.Data;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Repository
+{
+    public class HotelRoomRepository : IHotelRoomRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public HotelRoomRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public Task<HotelRoomDTO> CreateHotelRoom(HotelRoomDTO hotelRoomDTO)
+        {
+            var addedHotelRoom = _db.HotelRooms.Add(hotelRoomDTO);
+        }
+
+        public Task<IEnumerable<HotelRoomDTO>> GetAllHotelRooms()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HotelRoomDTO> GetHotelRoom(int roomId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HotelRoomDTO> IsSameNameRoomAlreadyPresent(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HotelRoomDTO> UpdateHotelRoom(int roomId, HotelRoomDTO hotelRoomDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

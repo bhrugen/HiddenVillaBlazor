61c590633445cce96b825c598f0c61d5e1ade7ce
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-11-29 10:49:59 -0600

    Section 4 - Hotel Repository Interface

---
+++ Business/Repository/IRepository/IHotelRoomRepository.cs
@@ -0,0 +1,18 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Repository.IRepository
+{
+    public interface IHotelRoomRepository
+    {
+        public Task<HotelRoomDTO> CreateHotelRoom(HotelRoomDTO hotelRoomDTO);
+        public Task<HotelRoomDTO> UpdateHotelRoom(int roomId, HotelRoomDTO hotelRoomDTO);
+        public Task<HotelRoomDTO> GetHotelRoom(int roomId);
+        public Task<IEnumerable<HotelRoomDTO>> GetAllHotelRooms();
+        public Task<HotelRoomDTO> IsSameNameRoomAlreadyPresent(string name);
+    }
+}

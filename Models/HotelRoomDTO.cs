f941cfd06706f12b1bb82d50d9045e3eb27a5dc6
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-11-29 10:39:10 -0600

    Section 4 - Hotel Room DTO

---
+++ Models/HotelRoomDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class HotelRoomDTO
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter room name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter Occupancy")]
+        public int Occupancy { get; set; }
+        [Range(1, 3000, ErrorMessage = "Regular rate must be between 1 and 3000")]
+        public double RegularRate { get; set; }
+        public string Details { get; set; }
+        public string SqFt { get; set; }
+    }
+}

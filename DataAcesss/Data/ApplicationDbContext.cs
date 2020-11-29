332897a4e3b17330bd2a9f71bceccbdcb226556a
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-11-28 23:04:10 -0600

    Section 4 - Add ApplicationDbContext

---
+++ DataAcesss/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcesss.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+    }
+}

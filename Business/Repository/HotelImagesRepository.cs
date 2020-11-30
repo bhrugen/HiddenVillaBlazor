c5a7d23a00273ae876fddb91525e2cb938a04e27
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-11-30 00:12:41 -0600

    Section 7 - Implement Room Image Repository

---
+++ Business/Repository/HotelImagesRepository.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using Business.Repository.IRepository;
+using DataAcesss.Data;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Repository
+{
+    public class HotelImagesRepository : IHotelImagesRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IMapper _mapper;
+        public HotelImagesRepository(ApplicationDbContext db, IMapper mapper)
+        {
+            _mapper = mapper;
+            _db = db;
+        }
+
+        public async Task<int> CreateHotelRoomImage(HotelRoomImageDTO imageDTO)
+        {
+            var image = _mapper.Map<HotelRoomImageDTO, HotelRoomImage>(imageDTO);
+            await _db.HotelRoomImages.AddAsync(image);
+            return await _db.SaveChangesAsync();
+        }
+
+        public async Task<int> DeleteHotelRoomImageByImageId(int imageId)
+        {
+            var image = await _db.HotelRoomImages.FindAsync(imageId);
+            _db.HotelRoomImages.Remove(image);
+            return await _db.SaveChangesAsync();
+        }
+
+        public async Task<int> DeleteHotelRoomImageByRoomId(int roomId)
+        {
+            var imageList = await _db.HotelRoomImages.Where(x => x.RoomId == roomId).ToListAsync();
+            _db.HotelRoomImages.RemoveRange(imageList);
+            return await _db.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<HotelRoomImageDTO>> GetHotelRoomImages(int roomId)
+        {
+            return _mapper.Map<IEnumerable<HotelRoomImage>, IEnumerable<HotelRoomImageDTO>>(
+            await _db.HotelRoomImages.Where(x => x.RoomId == roomId).ToListAsync());
+        }
+    }
+}

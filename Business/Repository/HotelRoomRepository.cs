cb95e1bffccb8ce8a059947eb690ed6fdb7f8cec
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-11-30 10:45:57 -0600

    Section 7 - Display Image on Edit

--- Business/Repository/HotelRoomRepository.cs
+++ Business/Repository/HotelRoomRepository.cs
@@ -47,7 +47,8 @@ namespace Business.Repository
             try
             {
                 IEnumerable<HotelRoomDTO> hotelRoomDTOs =
-                            _mapper.Map<IEnumerable<HotelRoom>, IEnumerable<HotelRoomDTO>>(_db.HotelRooms);
+                            _mapper.Map<IEnumerable<HotelRoom>, IEnumerable<HotelRoomDTO>>
+                            (_db.HotelRooms.Include(x => x.HotelRoomImages));
 
                 return hotelRoomDTOs;
             }
@@ -62,7 +63,7 @@ namespace Business.Repository
             try
             {
                 HotelRoomDTO hotelRoom = _mapper.Map<HotelRoom, HotelRoomDTO>(
-                    await _db.HotelRooms.FirstOrDefaultAsync(x => x.Id == roomId));
+                    await _db.HotelRooms.Include(x => x.HotelRoomImages).FirstOrDefaultAsync(x => x.Id == roomId));
 
                 return hotelRoom;
             }

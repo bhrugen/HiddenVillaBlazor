f39f53e6bd98549665ca57273c00b7c4236f03a7
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-11-30 00:22:36 -0600

    Section 7 - File Upload Interface

---
+++ HiddenVilla_Server/Service/FileUpload.cs
@@ -0,0 +1,22 @@
+﻿using HiddenVilla_Server.Service.IService;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Server.Service
+{
+    public class FileUpload : IFileUpload
+    {
+        public bool DeleteFile(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> UploadFile(IBrowserFile file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HiddenVilla_Server/Service/IService/IFileUpload.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Server.Service.IService
+{
+    public interface IFileUpload
+    {
+        Task<string> UploadFile(IBrowserFile file);
+
+        bool DeleteFile(string fileName);
+    }
+}

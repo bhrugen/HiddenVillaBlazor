5f6a39a78b34d94b2d9db449b1ac44e84ad60f1d
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-11-30 00:36:54 -0600

    Section 7 - Implement File Upload Interface

--- HiddenVilla_Server/Service/FileUpload.cs
+++ HiddenVilla_Server/Service/FileUpload.cs
@@ -1,7 +1,9 @@
 ﻿using HiddenVilla_Server.Service.IService;
 using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,14 +11,60 @@ namespace HiddenVilla_Server.Service
 {
     public class FileUpload : IFileUpload
     {
-        public bool DeleteFile(string fileName)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public FileUpload(IWebHostEnvironment webHostEnvironment)
         {
-            throw new NotImplementedException();
+            _webHostEnvironment = webHostEnvironment;
         }
 
-        public Task<string> UploadFile(IBrowserFile file)
+        public bool DeleteFile(string fileName)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var path = $"{_webHostEnvironment.WebRootPath}\\RoomImages\\{fileName}";
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<string> UploadFile(IBrowserFile file)
+        {
+            try
+            {
+                FileInfo fileInfo = new FileInfo(file.Name);
+                var fileName = Guid.NewGuid().ToString() + fileInfo.Extension;
+                var folderDirectory = $"{_webHostEnvironment.WebRootPath}\\RoomImages";
+                var path = Path.Combine(_webHostEnvironment.WebRootPath, "RoomImages", fileName);
+
+                var memoryStream = new MemoryStream();
+                await file.OpenReadStream().CopyToAsync(memoryStream);
+
+                if (!Directory.Exists(folderDirectory))
+                {
+                    Directory.CreateDirectory(folderDirectory);
+                }
+
+                await using (var fs = new FileStream(path, FileMode.Create, FileAccess.Write))
+                {
+                    memoryStream.WriteTo(fs);
+                }
+
+                var fullPath = $"RoomImages/{fileName}";
+                return fullPath;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }

aee9630f458cdddc029e5cde01d1214d6ff0dc6d
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-11-29 20:40:13 -0600

    Section 6 - Assignment 7 Solution- Sweet Alerts

--- HiddenVilla_Server/wwwroot/js/common.js
+++ HiddenVilla_Server/wwwroot/js/common.js
@@ -5,4 +5,22 @@
     if (type === "error") {
         toastr.error(message, "Operation Failed", { timeOut: 10000 });
     }
+}
+
+
+window.ShowSwal = (type, message) => {
+    if (type === "success") {
+        Swal.fire(
+            'Success Notification',
+            message,
+            'success'
+        )
+    }
+    if (type === "error") {
+        Swal.fire(
+            'Error Notification',
+            message,
+            'error'
+        )
+    }
 } 

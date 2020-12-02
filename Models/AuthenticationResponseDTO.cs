0c80c8f12d805d53ef8a9f0b430a906d7b690ad0
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-02 14:26:20 -0600

    Section 10 - Sign In DTO's

---
+++ Models/AuthenticationResponseDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class AuthenticationResponseDTO
+    {
+        public bool IsAuthSuccessful { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+        public string Token { get; set; }
+        public UserDTO userDTO { get; set; }
+    }
+}
---
+++ Models/UserDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class UserDTO
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNo { get; set; }
+    }
+}

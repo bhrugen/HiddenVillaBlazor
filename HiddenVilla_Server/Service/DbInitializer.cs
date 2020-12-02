073103c783f3805458152f95527c0943bbac4d18
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-01 22:21:53 -0600

    Section 9 - DB Initializer Part 2

--- HiddenVilla_Server/Service/DbInitializer.cs
+++ HiddenVilla_Server/Service/DbInitializer.cs
@@ -1,6 +1,8 @@
-﻿using DataAcesss.Data;
+﻿using Common;
+using DataAcesss.Data;
 using HiddenVilla_Server.Service.IService;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +27,33 @@ namespace HiddenVilla_Server.Service
 
         public void Initalize()
         {
+            try
+            {
+                if (_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+
+            if (_db.Roles.Any(x => x.Name == SD.Role_Admin)) return;
+
+            _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+            _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
+            _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
+
+            _userManager.CreateAsync(new IdentityUser
+            {
+                UserName = "admin@gmail.com",
+                Email = "admin@gmail.com",
+                EmailConfirmed = true
+            }, "Admin123*").GetAwaiter().GetResult();
+
+            IdentityUser user = _db.Users.FirstOrDefault(u => u.Email == "admin@gmail.com");
+            _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
 
         }
     }

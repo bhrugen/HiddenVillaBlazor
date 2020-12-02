3bef3ad6f303e78483186b3958bae39bf74f69f0
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-02 11:35:01 -0600

    Section 10 - Hotel Room Controller

---
+++ HiddenVilla_Api/Controllers/HotelRoomController.cs
@@ -0,0 +1,27 @@
+﻿using Business.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class HotelRoomController : Controller
+    {
+        private readonly IHotelRoomRepository _hotelRoomRepository;
+
+        public HotelRoomController(IHotelRoomRepository hotelRoomRepository)
+        {
+            _hotelRoomRepository = hotelRoomRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetHotelRooms()
+        {
+            var allRooms = await _hotelRoomRepository.GetAllHotelRooms();
+            return Ok(allRooms);
+        }
+    }
+}

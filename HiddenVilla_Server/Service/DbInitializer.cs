e8555e84f3d29915a92d47fa59130e284334e5d3
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-01 21:10:50 -0600

    Section 9 - DB Initializer Part 1

---
+++ HiddenVilla_Server/Service/DbInitializer.cs
@@ -0,0 +1,31 @@
+﻿using DataAcesss.Data;
+using HiddenVilla_Server.Service.IService;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Server.Service
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+
+        public DbInitializer(ApplicationDbContext db, UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public void Initalize()
+        {
+
+        }
+    }
+}
---
+++ HiddenVilla_Server/Service/IService/IDbInitializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Server.Service.IService
+{
+    public interface IDbInitializer
+    {
+        void Initalize();
+    }
+}

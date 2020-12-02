27539ed30a7da0b54e23af4bf2f486c97a9e7647
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-02 13:43:00 -0600

    Section 10 - Registeration DTO

---
+++ Models/RegisterationResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class RegisterationResponseDTO
+    {
+        public bool IsRegisterationSuccessful { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+    }
+}

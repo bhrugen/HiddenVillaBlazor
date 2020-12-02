a478d7064bb23676de5c91c709dbb2ebee71acb6
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-01 21:03:49 -0600

    Section 8 - Adding roles access to pages

---
+++ Common/SD.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public static class SD
+    {
+        public const string Role_Admin = "Admin";
+        public const string Role_Customer = "Customer";
+        public const string Role_Employee = "Employee";
+    }
+}

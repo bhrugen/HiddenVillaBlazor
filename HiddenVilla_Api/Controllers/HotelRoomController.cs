04559ba248ab96c3236a53864b96aebf5ee64db8
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-02 11:44:23 -0600

    Section 10 - Get Hotel Room EndPoint

--- HiddenVilla_Api/Controllers/HotelRoomController.cs
+++ HiddenVilla_Api/Controllers/HotelRoomController.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Repository.IRepository;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,6 +24,34 @@ namespace HiddenVilla_Api.Controllers
         {
             var allRooms = await _hotelRoomRepository.GetAllHotelRooms();
             return Ok(allRooms);
+        }
+
+        [HttpGet("{roomId}")]
+        public async Task<IActionResult> GetHotelRoom(int? roomId)
+        {
+            if (roomId == null)
+            {
+                return BadRequest(new ErrorModel()
+                {
+                    Title = "",
+                    ErrorMessage = "Invalid Room Id",
+                    StatusCode = StatusCodes.Status400BadRequest
+                });
+            }
+
+            var roomDetails = await _hotelRoomRepository.GetHotelRoom(roomId.Value);
+            if (roomDetails == null)
+            {
+                return BadRequest(new ErrorModel()
+                {
+                    Title = "",
+                    ErrorMessage = "Invalid Room Id",
+                    StatusCode = StatusCodes.Status404NotFound
+                });
+            }
+
+            return Ok(roomDetails);
+
         }
     }
 }
---
+++ Models/ErrorModel.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class ErrorModel
+    {
+        public string Title { get; set; }
+        public int StatusCode { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}

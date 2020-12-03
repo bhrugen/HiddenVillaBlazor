d3e1b1f47b3acfe9904f85ac9236e01ec175a147
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-03 09:41:53 -0600

    Section 12 - Add Checkin and Checkout to Hotel Room API

--- Business/Repository/IRepository/IHotelRoomRepository.cs
+++ Business/Repository/IRepository/IHotelRoomRepository.cs
@@ -11,9 +11,9 @@ namespace Business.Repository.IRepository
     {
         public Task<HotelRoomDTO> CreateHotelRoom(HotelRoomDTO hotelRoomDTO);
         public Task<HotelRoomDTO> UpdateHotelRoom(int roomId, HotelRoomDTO hotelRoomDTO);
-        public Task<HotelRoomDTO> GetHotelRoom(int roomId);
+        public Task<HotelRoomDTO> GetHotelRoom(int roomId, string checkInDate = null, string checkOutDate = null);
         public Task<int> DeleteHotelRoom(int roomId);
-        public Task<IEnumerable<HotelRoomDTO>> GetAllHotelRooms();
+        public Task<IEnumerable<HotelRoomDTO>> GetAllHotelRooms(string checkInDate = null, string checkOutDate = null);
         public Task<HotelRoomDTO> IsRoomUnique(string name, int roomId = 0);
     }
 }
--- HiddenVilla_Api/Controllers/HotelRoomController.cs
+++ HiddenVilla_Api/Controllers/HotelRoomController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Models;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -20,14 +21,39 @@ namespace HiddenVilla_Api.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetHotelRooms()
+        public async Task<IActionResult> GetHotelRooms(string checkInDate = null, string checkOutDate = null)
         {
-            var allRooms = await _hotelRoomRepository.GetAllHotelRooms();
+            if (string.IsNullOrEmpty(checkInDate) || string.IsNullOrEmpty(checkOutDate))
+            {
+                return BadRequest(new ErrorModel()
+                {
+                    StatusCode = StatusCodes.Status400BadRequest,
+                    ErrorMessage = "All parameters need to be supplied"
+                });
+            }
+            if (!DateTime.TryParseExact(checkInDate, "MM/dd/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out var dtCheckInDate))
+            {
+                return BadRequest(new ErrorModel()
+                {
+                    StatusCode = StatusCodes.Status400BadRequest,
+                    ErrorMessage = "Invalid CheckIn date format. valid format will be MM/dd/yyyy"
+                });
+            }
+            if (!DateTime.TryParseExact(checkOutDate, "MM/dd/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out var dtCheckOutDate))
+            {
+                return BadRequest(new ErrorModel()
+                {
+                    StatusCode = StatusCodes.Status400BadRequest,
+                    ErrorMessage = "Invalid CheckOut date format. valid format will be MM/dd/yyyy"
+                });
+            }
+
+            var allRooms = await _hotelRoomRepository.GetAllHotelRooms(checkInDate, checkOutDate);
             return Ok(allRooms);
         }
 
         [HttpGet("{roomId}")]
-        public async Task<IActionResult> GetHotelRoom(int? roomId)
+        public async Task<IActionResult> GetHotelRoom(int? roomId, string checkInDate = null, string checkOutDate = null)
         {
             if (roomId == null)
             {

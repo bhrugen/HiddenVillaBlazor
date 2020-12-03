e241917a22254425975477d7ecca999d53d07596
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-02 23:10:09 -0600

    Section 11 - Home View Model

---
+++ HiddenVilla_Client/Model/ViewModel/HomeVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Client.Model.ViewModel
+{
+    public class HomeVM
+    {
+        public DateTime StartDate { get; set; } = DateTime.Now;
+        public DateTime EndDate { get; set; }
+        public int NoOfNights { get; set; } = 1;
+    }
+}

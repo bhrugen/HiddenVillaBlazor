c032ddd40fd6be1c0a633eebf81b089f617f1380
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-03 09:32:35 -0600

    Section 12 - Get Hotel Rooms Service Implementation

---
+++ HiddenVilla_Client/Service/HotelRoomService.cs
@@ -0,0 +1,34 @@
+﻿using HiddenVilla_Client.Service.IService;
+using Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Client.Service
+{
+    public class HotelRoomService : IHotelRoomService
+    {
+        private readonly HttpClient _client;
+
+        public HotelRoomService(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public Task<HotelRoomDTO> GetHotelRoomDetails(int roomId, string checkInDate, string checkOutDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<HotelRoomDTO>> GetHotelRooms(string checkInDate, string checkOutDate)
+        {
+            var response = await _client.GetAsync($"api/hotelroom?checkInDate={checkInDate}&checkOutDate={checkOutDate}");
+            var content = await response.Content.ReadAsStringAsync();
+            var rooms = JsonConvert.DeserializeObject<IEnumerable<HotelRoomDTO>>(content);
+            return rooms;
+        }
+    }
+}

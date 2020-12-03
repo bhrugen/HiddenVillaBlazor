39606d0eea84d46999c6eb29e2b296cb34e540d4
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-02 23:29:39 -0600

    Section 11 - Local Storage in Action

--- Common/SD.cs
+++ Common/SD.cs
@@ -11,5 +11,7 @@ namespace Common
         public const string Role_Admin = "Admin";
         public const string Role_Customer = "Customer";
         public const string Role_Employee = "Employee";
+
+        public const string Local_InitialBooking = "InitialRoomBookingInfo";
     }
 }

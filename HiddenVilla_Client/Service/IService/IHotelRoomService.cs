c8d754a0fd2833c92bcbcd6bc96fc3fd6c53b016
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-03 09:23:05 -0600

    Section 12 - Hotel Room Interface

---
+++ HiddenVilla_Client/Service/IService/IHotelRoomService.cs
@@ -0,0 +1,14 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Client.Service.IService
+{
+    public interface IHotelRoomService
+    {
+        public Task<IEnumerable<HotelRoomDTO>> GetHotelRooms(string checkInDate, string checkOutDate);
+        public Task<HotelRoomDTO> GetHotelRoomDetails(int roomId, string checkInDate, string checkOutDate);
+    }
+}

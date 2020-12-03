f93f600cb19e4546f9c28f8f7966f3f079853387
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-02 23:23:18 -0600

    Section 11 - Local Storage

--- HiddenVilla_Client/Program.cs
+++ HiddenVilla_Client/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,7 +19,7 @@ namespace HiddenVilla_Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.AddBlazoredLocalStorage();
             await builder.Build().RunAsync();
         }
     }

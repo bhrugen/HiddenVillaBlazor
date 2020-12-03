b42eb9aa1e2aec6337fe54e4cfef8da22b9a0a8b
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-03 16:06:57 -0600

    Section 12 - Add more properties to HotelRoomDTO

--- Models/HotelRoomDTO.cs
+++ Models/HotelRoomDTO.cs
@@ -18,6 +18,10 @@ namespace Models
         public double RegularRate { get; set; }
         public string Details { get; set; }
         public string SqFt { get; set; }
+
+        public double TotalDays { get; set; }
+        public double TotalAmount { get; set; }
+
         public virtual ICollection<HotelRoomImageDTO> HotelRoomImages { get; set; }
 
         public List<string> ImageUrls { get; set; }

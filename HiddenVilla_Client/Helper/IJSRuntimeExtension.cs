10e1c79396792e758fbee38fed5f58508f6f4787
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-02 22:32:37 -0600

    Section 11 - Add Toastr, setup css and js cdn

---
+++ HiddenVilla_Client/Helper/IJSRuntimeExtension.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Client.Helper
+{
+    public static class IJSRuntimeExtension
+    {
+        public static async ValueTask ToastrSuccess(this IJSRuntime JSRuntime, string message)
+        {
+            await JSRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+        }
+        public static async ValueTask ToastrError(this IJSRuntime JSRuntime, string message)
+        {
+            await JSRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+        }
+    }
+}
---
+++ HiddenVilla_Client/wwwroot/js/common.js
@@ -0,0 +1,8 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, "Operation Successful", { timeOut: 10000 });
+    }
+    if (type === "error") {
+        toastr.error(message, "Operation Failed", { timeOut: 10000 });
+    }
+}

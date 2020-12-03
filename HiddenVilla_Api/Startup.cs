16599a49ee50fa883a8421772e2225685216aef9
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-02 19:20:57 -0600

    Section 10 - Add Bearer to Swagger

--- HiddenVilla_Api/Startup.cs
+++ HiddenVilla_Api/Startup.cs
@@ -91,6 +91,26 @@ namespace HiddenVilla_Api
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "HiddenVilla_Api", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please Bearer and then token in the field",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                   {
+                     new OpenApiSecurityScheme
+                     {
+                       Reference = new OpenApiReference
+                       {
+                         Type = ReferenceType.SecurityScheme,
+                         Id = "Bearer"
+                       }
+                      },
+                      new string[] { }
+                    }
+                });
             });
         }
 

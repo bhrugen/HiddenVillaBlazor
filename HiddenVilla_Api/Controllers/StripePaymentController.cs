f0afff830ab3ecd3f279f62a720bc6b694b6b16f
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-04 15:24:47 -0600

    Section 14 - Create Stripe Endpoint in API

--- HiddenVilla_Api/Controllers/StripePaymentController.cs
+++ HiddenVilla_Api/Controllers/StripePaymentController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Models;
+using Stripe.Checkout;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,9 +20,56 @@ namespace HiddenVilla_Api.Controllers
             _configuration = configuration;
         }
 
-        public IActionResult Index()
+        [HttpPost]
+        public async Task<IActionResult> Create(StripePaymentDTO payment)
         {
-            return View();
+            try
+            {
+                var domain = _configuration.GetValue<string>("HiddenVilla_Client_URL");
+
+                var options = new SessionCreateOptions
+                {
+                    PaymentMethodTypes = new List<string>
+                    {
+                        "card"
+                    },
+                    LineItems = new List<SessionLineItemOptions>
+                    {
+                        new SessionLineItemOptions
+                        {
+                            PriceData = new SessionLineItemPriceDataOptions
+                            {
+                                UnitAmount=payment.Amount*100,//convert to cents
+                                Currency="usd",
+                                ProductData= new SessionLineItemPriceDataProductDataOptions
+                                {
+                                    Name = payment.ProductName
+                                }
+                            },
+                            Quantity=1
+                        }
+                    },
+                    Mode = "payment",
+                    SuccessUrl = domain + "/success-payment?session_id={{CHECKOUT_SESSION_ID}}",
+                    CancelUrl = domain + payment.ReturnUrl
+                };
+
+                var service = new SessionService();
+                Session session = await service.CreateAsync(options);
+
+                return Ok(new SuccessModel()
+                {
+                    Data = session.Id
+                });
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new ErrorModel()
+                {
+                    ErrorMessage = e.Message
+                });
+            }
         }
     }
 }

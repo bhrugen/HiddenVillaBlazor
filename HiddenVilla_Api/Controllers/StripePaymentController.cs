4aa773aa97bdea0bd381d98b624530882e660654
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-04 12:47:58 -0600

    Section 14 - Stripe API Endpoint

---
+++ HiddenVilla_Api/Controllers/StripePaymentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class StripePaymentController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public StripePaymentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

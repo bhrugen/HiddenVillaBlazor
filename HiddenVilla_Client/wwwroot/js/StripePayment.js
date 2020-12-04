365300c68a4299f402357044645b014f28d32f7c
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-04 16:07:55 -0600

    Section 14 - Invoke Stripe

---
+++ HiddenVilla_Client/wwwroot/js/StripePayment.js
@@ -0,0 +1,6 @@
+﻿redirectToCheckout = function (sessionId) {
+    var stripe = Stripe('pk_test_51HuidRES6XguoEyXQbu9mQoeVsUgj2vZu5KcspDQd3fPojBsftW15gTQw4GjQ6Jt4AlCLKzta7iHAKp3Ccfi6jYr00mmbTUIZR');
+    stripe.redirectToCheckout({
+        sessionId: sessionId
+    });
+};

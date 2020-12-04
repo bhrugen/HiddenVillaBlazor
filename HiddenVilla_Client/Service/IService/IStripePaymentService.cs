0ecd0dd5ae85069571a55b05acda23bdaa697bee
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-04 15:32:56 -0600

    Section 14 - Stripe Payment Service

---
+++ HiddenVilla_Client/Service/IService/IStripePaymentService.cs
@@ -0,0 +1,13 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Client.Service.IService
+{
+    public interface IStripePaymentService
+    {
+        public Task<SuccessModel> CheckOut(StripePaymentDTO model);
+    }
+}
---
+++ HiddenVilla_Client/Service/StripePaymentService.cs
@@ -0,0 +1,41 @@
+﻿using HiddenVilla_Client.Service.IService;
+using Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Client.Service
+{
+    public class StripePaymentService : IStripePaymentService
+    {
+        private readonly HttpClient _client;
+
+        public StripePaymentService(HttpClient client)
+        {
+            _client = client;
+        }
+        public async Task<SuccessModel> CheckOut(StripePaymentDTO model)
+        {
+            var content = JsonConvert.SerializeObject(model);
+            var bodyContent = new StringContent(content, Encoding.UTF8, "application/json");
+            var response = await _client.PostAsync("api/stripepayment/create", bodyContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var contentTemp = await response.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<SuccessModel>(contentTemp);
+                return result;
+            }
+            else
+            {
+                var contentTemp = await response.Content.ReadAsStringAsync();
+                var errorModel = JsonConvert.DeserializeObject<ErrorModel>(contentTemp);
+                throw new Exception(errorModel.ErrorMessage);
+            }
+        }
+    }
+}

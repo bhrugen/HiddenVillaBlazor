e5602206d9555cb251ea54bfe304d0236be1bfb0
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-04 10:16:09 -0600

    Section 13 - Room Details On Initialized Method Part 1

---
+++ HiddenVilla_Client/Model/ViewModel/HotelRoomBookingVM.cs
@@ -0,0 +1,13 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Client.Model.ViewModel
+{
+    public class HotelRoomBookingVM
+    {
+        public RoomOrderDetailsDTO OrderDetails { get; set; }
+    }
+}

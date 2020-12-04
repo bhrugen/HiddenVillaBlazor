2257dc062571602dcc416cc7542f7981b3ccf975
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-04 11:49:51 -0600

    Section 14 - Add Stripe DTO Models

---
+++ Models/StripePaymentDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class StripePaymentDTO
+    {
+        public string ProductName { get; set; }
+        public long Amount { get; set; }
+        public string ImageUrl { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}

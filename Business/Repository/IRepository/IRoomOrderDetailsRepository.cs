974a8c6988681b58759035febd9c330296ace781
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-03 23:39:09 -0600

    Section 13 - Room Order Details Repository Interface

---
+++ Business/Repository/IRepository/IRoomOrderDetailsRepository.cs
@@ -0,0 +1,19 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Repository.IRepository
+{
+    public interface IRoomOrderDetailsRepository
+    {
+        public Task<RoomOrderDetailsDTO> Create(RoomOrderDetailsDTO details);
+        public Task<RoomOrderDetailsDTO> MarkPaymentSuccessful(int id);
+        public Task<RoomOrderDetailsDTO> GetRoomOrderDetail(int roomOrderId);
+        public Task<IEnumerable<RoomOrderDetailsDTO>> GetAllRoomOrderDetails();
+        public Task<bool> UpdateOrderStatus(int RoomOrderId, string status);
+        public Task<bool> IsRoomBooked(int RoomId, DateTime checkInDate, DateTime checkOutDate);
+    }
+}

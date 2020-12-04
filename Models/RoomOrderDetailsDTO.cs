5b9a02a9cd916133abc9d16ffb48623ea8388e48
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-03 23:32:48 -0600

    Section 13 - Room Order Details DTO

---
+++ Models/RoomOrderDetailsDTO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class RoomOrderDetailsDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        [Required]
+        public string StripeSessionId { get; set; }
+        [Required]
+        public DateTime CheckInDate { get; set; }
+        [Required]
+        public DateTime CheckOutDate { get; set; }
+        public DateTime ActualCheckInDate { get; set; }
+        public DateTime ActualCheckOutDate { get; set; }
+        [Required]
+        public long TotalCost { get; set; }
+        [Required]
+        public int RoomId { get; set; }
+        public bool IsPaymentSuccessful { get; set; } = false;
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public HotelRoomDTO HotelRoomDTO { get; set; }
+        public string Status { get; set; }
+    }
+}

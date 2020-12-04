a9b02fe754262fc7fe8f6d2a152dd36b9da5f014
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-04 15:42:34 -0600

    Section 14 - Room Order Endpoint in API

---
+++ HiddenVilla_Api/Controllers/RoomOrderController.cs
@@ -0,0 +1,39 @@
+﻿using Business.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class RoomOrderController : Controller
+    {
+        private readonly IRoomOrderDetailsRepository _repository;
+
+        public RoomOrderController(IRoomOrderDetailsRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] RoomOrderDetailsDTO details)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _repository.Create(details);
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(new ErrorModel()
+                {
+                    ErrorMessage = "Error while creating Room Details/ Booking"
+                });
+            }
+        }
+    }
+}
--- HiddenVilla_Client/Service/RoomOrderDetailsService.cs
+++ HiddenVilla_Client/Service/RoomOrderDetailsService.cs
@@ -1,9 +1,11 @@
 ﻿using HiddenVilla_Client.Service.IService;
 using Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace HiddenVilla_Client.Service
@@ -21,9 +23,24 @@ namespace HiddenVilla_Client.Service
             throw new NotImplementedException();
         }
 
-        public Task<RoomOrderDetailsDTO> SaveRoomOrderDetails(RoomOrderDetailsDTO details)
+        public async Task<RoomOrderDetailsDTO> SaveRoomOrderDetails(RoomOrderDetailsDTO details)
         {
-            throw new NotImplementedException();
+            var content = JsonConvert.SerializeObject(details);
+            var bodyContent = new StringContent(content, Encoding.UTF8, "application/json");
+            var response = await _client.PostAsync("api/roomorder/create", bodyContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var contentTemp = await response.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<RoomOrderDetailsDTO>(contentTemp);
+                return result;
+            }
+            else
+            {
+                var contentTemp = await response.Content.ReadAsStringAsync();
+                var errorModel = JsonConvert.DeserializeObject<ErrorModel>(contentTemp);
+                throw new Exception(errorModel.ErrorMessage);
+            }
         }
     }
 }

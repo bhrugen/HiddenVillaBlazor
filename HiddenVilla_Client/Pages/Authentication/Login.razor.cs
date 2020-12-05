212a17dfaa1cf4ae7e8f99cc592a35272dc772ee
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-05 13:22:38 -0600

    Section 13 - Login Demo

---
+++ HiddenVilla_Client/Pages/Authentication/Login.razor.cs
@@ -0,0 +1,54 @@
+﻿using HiddenVilla_Client.Service.IService;
+using Microsoft.AspNetCore.Components;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace HiddenVilla_Client.Pages.Authentication
+{
+    public partial class Login
+    {
+        private AuthenticationDTO UserForAuthentication = new AuthenticationDTO();
+        public bool IsProcessing { get; set; } = false;
+        public bool ShowAuthenticationErrors { get; set; }
+        public string Errors { get; set; }
+        public string ReturnUrl { get; set; }
+        [Inject]
+        public IAuthenticationService authenticationService { get; set; }
+
+        [Inject]
+        public NavigationManager navigationManager { get; set; }
+
+        private async Task LoginUser()
+        {
+            ShowAuthenticationErrors = false;
+            IsProcessing = true;
+            var result = await authenticationService.Login(UserForAuthentication);
+            if (result.IsAuthSuccessful)
+            {
+                IsProcessing = false;
+
+                var absoluteUri = new Uri(navigationManager.Uri);
+                var queryParam = HttpUtility.ParseQueryString(absoluteUri.Query);
+                ReturnUrl = queryParam["returnUrl"];
+                if (string.IsNullOrEmpty(ReturnUrl))
+                {
+                    navigationManager.NavigateTo("/");
+                }
+                else
+                {
+                    navigationManager.NavigateTo("/" + ReturnUrl);
+                }
+            }
+            else
+            {
+                IsProcessing = false;
+                Errors = result.ErrorMessage;
+                ShowAuthenticationErrors = true;
+            }
+        }
+    }
+}
---
+++ HiddenVilla_Client/Pages/Authentication/Register.razor.cs
@@ -0,0 +1,42 @@
+﻿using HiddenVilla_Client.Service.IService;
+using Microsoft.AspNetCore.Components;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Client.Pages.Authentication
+{
+    public partial class Register
+    {
+        private UserRequestDTO UserForRegisteration = new UserRequestDTO();
+        public bool IsProcessing { get; set; } = false;
+        public bool ShowRegistrationErrors { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+
+        [Inject]
+        public IAuthenticationService authenticationService { get; set; }
+
+        [Inject]
+        public NavigationManager navigationManager { get; set; }
+
+        private async Task RegisterUser()
+        {
+            ShowRegistrationErrors = false;
+            IsProcessing = true;
+            var result = await authenticationService.RegisterUser(UserForRegisteration);
+            if (result.IsRegisterationSuccessful)
+            {
+                IsProcessing = false;
+                navigationManager.NavigateTo("/login");
+            }
+            else
+            {
+                IsProcessing = false;
+                Errors = result.Errors;
+                ShowRegistrationErrors = true;
+            }
+        }
+    }
+}

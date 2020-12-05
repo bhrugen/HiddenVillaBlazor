5d81cdd632f847d79c60bc035bc3b99360e54cce
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-05 11:31:29 -0600

    Section 15 - IAuthenticationService

---
+++ HiddenVilla_Client/Service/IService/IAuthenticationService.cs
@@ -0,0 +1,17 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Client.Service.IService
+{
+    public interface IAuthenticationService
+    {
+        Task<RegisterationResponseDTO> RegisterUser(UserRequestDTO userForRegisteration);
+
+        Task<AuthenticationResponseDTO> Login(AuthenticationDTO userFromAuthentication);
+
+        Task Logout();
+    }
+}

ee74bac3029f0991ed84d85de61386975e6a3bbc
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-05 14:19:08 -0600

    Section 15 - UserInfo from LocalStorage

--- HiddenVilla_Client/Service/RoomOrderDetailsService.cs
+++ HiddenVilla_Client/Service/RoomOrderDetailsService.cs
@@ -40,7 +40,6 @@ namespace HiddenVilla_Client.Service
 
         public async Task<RoomOrderDetailsDTO> SaveRoomOrderDetails(RoomOrderDetailsDTO details)
         {
-            details.UserId = "dummy user";
             var content = JsonConvert.SerializeObject(details);
             var bodyContent = new StringContent(content, Encoding.UTF8, "application/json");
             var response = await _client.PostAsync("api/roomorder/create", bodyContent);

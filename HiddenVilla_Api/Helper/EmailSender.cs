af05b59e7bb4b42880cf388e72cacc8054ddf746
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-05 15:09:54 -0600

    Section 16 - EmailSender

---
+++ HiddenVilla_Api/Helper/EmailSender.cs
@@ -0,0 +1,52 @@
+﻿using Mailjet.Client;
+using Mailjet.Client.Resources;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Api.Helper
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly MailJetSettings _mailJetSettings;
+
+        public EmailSender(IOptions<MailJetSettings> mailjetSettings)
+        {
+            _mailJetSettings = mailjetSettings.Value;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            MailjetClient client = new MailjetClient(Environment.GetEnvironmentVariable(_mailJetSettings.PublicKey),
+                Environment.GetEnvironmentVariable(_mailJetSettings.PrivateKey))
+            {
+                Version = ApiVersion.V3_1,
+            };
+            MailjetRequest request = new MailjetRequest
+            {
+                Resource = Send.Resource,
+            }
+               .Property(Send.Messages, new JArray {
+                new JObject {
+                 {"From", new JObject {
+                  {"Email", _mailJetSettings.Email},
+                  {"Name", "Mailjet Pilot"}
+                  }},
+                 {"To", new JArray {
+                  new JObject {
+                   {"Email", email},
+                   {"Name", "Hello"}
+                   }
+                  }},
+                 {"Subject", subject},
+                 {"HTMLPart", htmlMessage}
+                 }
+                   });
+            MailjetResponse response = await client.PostAsync(request);
+        }
+    }
+}

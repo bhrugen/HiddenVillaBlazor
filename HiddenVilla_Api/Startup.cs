469226ff9a3597bdb8e194cf1e7b9559a7eae33b
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-04 19:31:48 -0600

    Revert "a check"
    This reverts commit e7820307529a11ccae27603061a44c3cf66bd1cf.

--- HiddenVilla_Api/Startup.cs
+++ HiddenVilla_Api/Startup.cs
@@ -17,7 +17,6 @@ using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json.Serialization;
-using Stripe;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -119,8 +118,6 @@ namespace HiddenVilla_Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            StripeConfiguration.ApiKey = Configuration.GetSection("Stripe")["ApiKey"];
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
---
+++ HiddenVilla_Server/Model/BlazorRoom.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Server.Model
+{
+    public class BlazorRoom
+    {
+        public int Id { get; set; }
+        public string RoomName { get; set; }
+        public double Price { get; set; }
+        public bool IsActive { get; set; }
+        public List<BlazorRoomProp> RoomProps { get; set; }
+    }
+}

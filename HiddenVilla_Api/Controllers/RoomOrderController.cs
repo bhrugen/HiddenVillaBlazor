174c67c940f738fd74f8e688a3fc928dd6600ddb
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-05 00:09:05 -0600

    Section 14 - Mark Payment Successful API

--- HiddenVilla_Api/Controllers/RoomOrderController.cs
+++ HiddenVilla_Api/Controllers/RoomOrderController.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Repository.IRepository;
 using Microsoft.AspNetCore.Mvc;
 using Models;
+using Stripe.Checkout;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -34,6 +35,34 @@ namespace HiddenVilla_Api.Controllers
                     ErrorMessage = "Error while creating Room Details/ Booking"
                 });
             }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PaymentSuccessful([FromBody] RoomOrderDetailsDTO details)
+        {
+
+            var service = new SessionService();
+            var sessionDetails = service.Get(details.StripeSessionId);
+            if (sessionDetails.PaymentStatus == "paid")
+            {
+                var result = await _repository.MarkPaymentSuccessful(details.Id);
+                if (result == null)
+                {
+                    return BadRequest(new ErrorModel()
+                    {
+                        ErrorMessage = "Can not mark payment as successful"
+                    });
+                }
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(new ErrorModel()
+                {
+                    ErrorMessage = "Can not mark payment as successful"
+                });
+            }
+
         }
     }
 }
--- HiddenVilla_Client/Service/RoomOrderDetailsService.cs
+++ HiddenVilla_Client/Service/RoomOrderDetailsService.cs
@@ -18,9 +18,24 @@ namespace HiddenVilla_Client.Service
         {
             _client = client;
         }
-        public Task<RoomOrderDetailsDTO> MarkPaymentSuccessful(RoomOrderDetailsDTO details)
+        public async Task<RoomOrderDetailsDTO> MarkPaymentSuccessful(RoomOrderDetailsDTO details)
         {
-            throw new NotImplementedException();
+            var content = JsonConvert.SerializeObject(details);
+            var bodyContent = new StringContent(content, Encoding.UTF8, "application/json");
+            var response = await _client.PostAsync("api/roomorder/paymentsuccessful", bodyContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var contentTemp = await response.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<RoomOrderDetailsDTO>(contentTemp);
+                return result;
+            }
+            else
+            {
+                var contentTemp = await response.Content.ReadAsStringAsync();
+                var errorModel = JsonConvert.DeserializeObject<ErrorModel>(contentTemp);
+                throw new Exception(errorModel.ErrorMessage);
+            }
         }
 
         public async Task<RoomOrderDetailsDTO> SaveRoomOrderDetails(RoomOrderDetailsDTO details)

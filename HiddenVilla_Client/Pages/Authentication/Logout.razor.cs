f23b156f65f67bd3792b2902fa4da1374572f141
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-05 13:31:17 -0600

    Section 15 - Logout Demo

---
+++ HiddenVilla_Client/Pages/Authentication/Logout.razor.cs
@@ -0,0 +1,23 @@
+﻿using HiddenVilla_Client.Service.IService;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Client.Pages.Authentication
+{
+    public partial class Logout
+    {
+        [Inject]
+        public IAuthenticationService authenticationService { get; set; }
+        [Inject]
+        public NavigationManager navigationManager { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            await authenticationService.Logout();
+            navigationManager.NavigateTo("/");
+        }
+    }
+}

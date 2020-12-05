86043b8c959096f88ee7e76c01b9fbf3ac7f77d6
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-04 22:44:19 -0600

    Section 14 - Stripe Demo

--- HiddenVilla_Client/Service/RoomOrderDetailsService.cs
+++ HiddenVilla_Client/Service/RoomOrderDetailsService.cs
@@ -25,10 +25,11 @@ namespace HiddenVilla_Client.Service
 
         public async Task<RoomOrderDetailsDTO> SaveRoomOrderDetails(RoomOrderDetailsDTO details)
         {
+            details.UserId = "dummy user";
             var content = JsonConvert.SerializeObject(details);
             var bodyContent = new StringContent(content, Encoding.UTF8, "application/json");
             var response = await _client.PostAsync("api/roomorder/create", bodyContent);
-
+            //string res = response.Content.ReadAsStringAsync().Result;
             if (response.IsSuccessStatusCode)
             {
                 var contentTemp = await response.Content.ReadAsStringAsync();

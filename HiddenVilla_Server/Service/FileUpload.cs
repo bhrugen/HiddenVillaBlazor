358a66b97e83c582b044ad0727915d95846bc7d9
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-06 15:07:52 -0600

    Section 17 - Fixing Blazor Server Deployed Website Issue

--- HiddenVilla_Server/Service/FileUpload.cs
+++ HiddenVilla_Server/Service/FileUpload.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -13,12 +14,12 @@ namespace HiddenVilla_Server.Service
     public class FileUpload : IFileUpload
     {
         private readonly IWebHostEnvironment _webHostEnvironment;
-        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IConfiguration _configuration;
 
-        public FileUpload(IWebHostEnvironment webHostEnvironment, IHttpContextAccessor httpContextAccessor)
+        public FileUpload(IWebHostEnvironment webHostEnvironment, IConfiguration configuration)
         {
             _webHostEnvironment = webHostEnvironment;
-            _httpContextAccessor = httpContextAccessor;
+            _configuration = configuration;
         }
 
         public bool DeleteFile(string fileName)
@@ -60,7 +61,7 @@ namespace HiddenVilla_Server.Service
                 {
                     memoryStream.WriteTo(fs);
                 }
-                var url = $"{_httpContextAccessor.HttpContext.Request.Scheme}://{_httpContextAccessor.HttpContext.Request.Host.Value}/";
+                var url = $"{_configuration.GetValue<string>("ServerUrl")}";
                 var fullPath = $"{url}RoomImages/{fileName}";
                 return fullPath;
             }

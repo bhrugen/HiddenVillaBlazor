2343f96384ed102e4a4204c8647cac0736026ed6
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-12-06 16:26:40 -0600

    Section 17 - Deploy API

--- HiddenVilla_Api/Startup.cs
+++ HiddenVilla_Api/Startup.cs
@@ -126,10 +126,15 @@ namespace HiddenVilla_Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HiddenVilla_Api v1"));
+                
             }
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "HiddenVilla_Api v1");
+                c.RoutePrefix = string.Empty;
+                }
+            );
             app.UseHttpsRedirection();
             app.UseCors("HiddenVilla");
             app.UseRouting();
